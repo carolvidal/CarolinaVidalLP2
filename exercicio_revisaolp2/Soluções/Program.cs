3e76920acfda6dcb95ff23a528ca61c1f043cedf
Carol Vidal <carolvidal931@gmail.com>
2017-06-12 18:23:37 -0300

    Quinta etapa do exercício e sub-rotinas adicionadas.

--- exercicio_revisaolp2/Soluções/Program.cs
+++ exercicio_revisaolp2/Soluções/Program.cs
@@ -8,71 +8,93 @@ namespace ExercicioRevisao
 {
     class Program
     {
-        struct Carro
+        public struct Carro
         {
             public string modelo;
             public int pot;
             public double km;
+            public string placa;
+            public string cor;
+            public string fabricante;
+            public int ano;
         }
 
         static void Main(string[] args)
         {
-            string final;
-            int carros;
-            Carro Carro1;
-            Console.WriteLine("Insira o número de carros");
-            carros = Convert.ToInt32(Console.ReadLine());
+
+            int carros = int.Parse(Console.ReadLine());
+
+            Carro[] Carros = new Carro[carros];
 
             for (int i = 0; i < carros; i++)
             {
-                Console.WriteLine("Insira o seu modelo");
-                Carro1.modelo = Console.ReadLine();
-                Console.WriteLine("Insira a quilometragem: ");
-                Carro1.km = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Insira a potência: ");
-                Carro1.pot = Convert.ToInt32(Console.ReadLine());
-
-                final = Sub(Carro1.modelo, Carro1.km, Carro1.pot);
-
-                Console.WriteLine(final);
+                Carros[i].modelo = Console.ReadLine();
+                Carros[i].km = Double.Parse(Console.ReadLine());
+                Carros[i].pot = Int32.Parse(Console.ReadLine());
+                Carros[i].placa = Console.ReadLine();
+                Carros[i].cor = (Console.ReadLine());
+                Carros[i].fabricante = (Console.ReadLine());
+                Carros[i].ano = Int32.Parse(Console.ReadLine());
 
             }
+
+            for (int i = 0; i < carros; i++)
+                Console.WriteLine(Classificar(Carros[i]));
         }
 
-            static string Sub(string modelo, double km, int pot)
+        public static string Classificar(Carro c)
+        {
+            string modsaida, potsaida;
+
+            if (c.km <= 5000)
             {
-                string modsaida, potsaida, final;
-
-                if (km <= 5000)
-                {
-                    modsaida = "Novo";
-                }
-                else if (km>5000 && km <= 30000)
-                {
-                 modsaida = "Seminovo";
-                }
-                else
-                {
-                    modsaida = "Velho";
-                }
-                if (pot > 200)
-                {
-                    potsaida = "Potente";
-                }
-                else if (pot >= 120 && pot <= 200)
-                {
-                    potsaida = "Forte";
-                }
-                else
-                    potsaida = "Popular";
-                   
-                final = String.Format("{0} - {1} - {2}", modelo, modsaida, potsaida);
-
-                return final;
+                modsaida = "Novo";
             }
-        
+            else if (c.km > 5000 && c.km <= 30000)
+            {
+                modsaida = "Seminovo";
+            }
+            else
+            {
+                modsaida = "Velho";
+            }
+
+            if (c.pot > 200)
+            {
+                potsaida = "Potente";
+            }
+            else if (c.pot >= 120 && c.pot <= 200)
+            {
+                potsaida = "Forte";
+            }
+            else
+                potsaida = "Popular";
+
+            return String.Format("{0} - {1} - {2}", c.modelo, modsaida, potsaida);
+
         }
 
+        public static void AterarModelo(Carro c)
+        {
+            c.km = Double.Parse(Console.ReadLine());
+        }
+
+        public static void AterarCor(Carro c)
+        {
+            c.cor = Console.ReadLine();
+        }
+
+        public static int CalcularTaxaDeUso(Carro c)
+        {
+            int anoAtual, modsaida;
+
+            anoAtual = Int32.Parse(Console.ReadLine());
+            modsaida = anoAtual - c.ano;
+            return modsaida;
+
+        }
     }
+
+}
 
 
