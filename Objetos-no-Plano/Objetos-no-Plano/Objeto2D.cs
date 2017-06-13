2155840b694fe6204812372536f4c85cfa6891b9
Carol Vidal <carolvidal931@gmail.com>
2017-06-13 09:13:16 -0300

    Exercício Objetos no Plano concluído.

---
+++ Objetos-no-Plano/Objetos-no-Plano/Objeto2D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Objeto2D
+{
+    public int X, Y;
+
+    public void MoverParaDireita()
+    {
+        X += 1;
+    }
+    public string Coordenadas()
+    {
+        return String.Format("{0}, {1}", X, Y);
+    }
+}
