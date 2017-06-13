d8da6827e0c156fb68ce3ae4544a29636b94569f
Carol Vidal <carolvidal931@gmail.com>
2017-06-13 09:39:22 -0300

    Parte 2 do exercício de Objetos no Plano.

--- Objetos-no-Plano/Objetos-no-Plano/Objeto2D.cs
+++ Objetos-no-Plano/Objetos-no-Plano/Objeto2D.cs
@@ -8,6 +8,20 @@ public class Objeto2D
     {
         X += 1;
     }
+    public void MoverParaEsquerda()
+    {
+        X -= 1;
+    }
+
+    public void MoverParaCima()
+    {
+        Y += 1;
+    }
+
+    public void MoverParaBaixo()
+    {
+        Y -= 1;
+    }
     public string Coordenadas()
     {
         return String.Format("{0}, {1}", X, Y);
--- Objetos-no-Plano/Objetos-no-Plano/Program.cs
+++ Objetos-no-Plano/Objetos-no-Plano/Program.cs
@@ -30,11 +30,20 @@ namespace Objetos_no_Plano
                 if (comando == ConsoleKey.RightArrow)
                     obj.MoverParaDireita();
 
+                else if (comando == ConsoleKey.LeftArrow)
+                    obj.MoverParaEsquerda();
+
+                else if (comando == ConsoleKey.UpArrow)
+                    obj.MoverParaCima();
+
+                else if (comando == ConsoleKey.DownArrow)
+                    obj.MoverParaBaixo();
+
                 Console.WriteLine(obj.Coordenadas());
 
                 Console.Write("Digite comando: ");
                 comando = Console.ReadKey().Key;
-            }
+             }
         }
     }
 }
