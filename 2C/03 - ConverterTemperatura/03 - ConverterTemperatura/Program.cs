5bac0f3b145ae53ddc33a667801bfc863de140ca
Carol Vidal <carolvidal931@gmail.com>
2017-08-13 20:48:58 -0300

    Exercício da conversão de temperaturas.

---
+++ 2C/03 - ConverterTemperatura/03 - ConverterTemperatura/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03___ConverterTemperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TC t = new TC();
+            int x = 1;
+
+            while (x != 0)
+            {
+                Console.WriteLine("1 - Nova temperatura");
+                Console.WriteLine("2 - Para Fahrenheit");
+                Console.WriteLine("3 - Para Kelvin");
+                Console.WriteLine("0 - Sair");
+                x = int.Parse(Console.ReadLine());
+
+                if (x != 0)
+                {
+                    Console.WriteLine("Temperatura:");
+                    t.valor = double.Parse(Console.ReadLine());
+
+                    if (x == 2)
+                        Console.WriteLine(t.EmFahrenheit());
+                    else if (x == 3)
+                        Console.WriteLine(t.EmKelvin());
+                }
+
+                else
+                    break;
+
+
+            }
+        }
+    }
+}
---
+++ 2C/03 - ConverterTemperatura/03 - ConverterTemperatura/TC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03___ConverterTemperatura
+{
+    class TC
+    {
+            public double valor;
+
+            public double EmFahrenheit()
+            {
+                return 1.8 * valor + 32;
+            }
+
+            public double EmKelvin()
+            {
+                return valor + 273;
+            }
+    }
+}
