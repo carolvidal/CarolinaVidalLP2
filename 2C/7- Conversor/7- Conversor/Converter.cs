c5e5b27de33e45c447952f546e615152dba04851
Carol Vidal <carolvidal931@gmail.com>
2017-09-22 18:36:54 -0300

    Exercício do Conversor adicionado.

---
+++ 2C/7- Conversor/7- Conversor/Converter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7__Conversor
+{
+    class Converter
+    {
+        public static void KgParaG(double k)
+        {
+            Console.WriteLine("{0} g", k * 1000);
+        }
+        public static void KgParaT(double k)
+        {
+            Console.WriteLine("{0} t", k / 1000);
+        }
+        public static void CParaFh(double c)
+        {
+            Console.WriteLine("{0} F°", (c * 0.55) + 32);
+        }
+        public static void CParaK(double c)
+        {
+            Console.WriteLine("{0} K", c + 273);
+        }
+        public static void FhParaC(double f)
+        {
+            Console.WriteLine("{0} C°", (f - 32) * 0.55);
+        }
+
+        public static void MParaKm(double m)
+        {
+            Console.WriteLine("{0} km", m * 1000);
+        }
+
+        public static void KmParaMil(double km)
+        {
+            Console.WriteLine("{0} mi", km * 0.621);
+        }
+
+    }
+}
---
+++ 2C/7- Conversor/7- Conversor/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7__Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            int x = 0, y = 0;
+            double valor;
+            while (x != 4)
+            {
+                Console.WriteLine("1- Converter massas\n2- Converter temperaturas\n3- Converter distâncias\n4- Sair");
+                x = int.Parse(Console.ReadLine());
+
+                if (x == 1)
+                {
+                    Console.WriteLine("1- Quilos em Gramas\n2- Quilos em Toneladas");
+                    y = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Insira o valor:");
+                    valor = int.Parse(Console.ReadLine());
+
+                    if (y == 1)
+                    {
+                        Converter.KgParaG(valor);
+                    }
+
+                    else if (y == 2)
+                    {
+                        Converter.KgParaT(valor);
+                    }
+
+                    else
+                        throw new System.InvalidOperationException("Operação inválida!");
+                }
+
+                else if (x == 2)
+                {
+                    Console.WriteLine("1- Celsius em Fahrenheit\n2- Celsius em Kelvin");
+                    y = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Insira o valor:");
+                    valor = int.Parse(Console.ReadLine());
+
+                    if (y == 1)
+                    {
+                        Converter.CParaFh(valor);
+                    }
+
+                    else if (y == 2)
+                    {
+                        Converter.CParaK(valor);
+                    }
+                    else
+                        throw new System.InvalidOperationException("Operação inválida!");
+                }
+
+                else if (x == 3)
+                {
+                    Console.WriteLine("1- Metros em Quilômetros\n2- Quilômetros em Milhas");
+                    y = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Insira o valor:");
+                    valor = int.Parse(Console.ReadLine());
+
+                    if (y == 1)
+                    {
+                        Converter.MParaKm(valor);
+                    }
+
+                    else if (y == 2)
+                    {
+                        Converter.KmParaMil(valor);
+                    }
+                    else
+                        throw new System.InvalidOperationException("Operação inválida!");
+                }
+
+                else if (x == 4)
+
+                    break;
+
+                else
+
+                    throw new System.InvalidOperationException("Operação inválida!");
+
+            }
+
+        }
+    }
+}
