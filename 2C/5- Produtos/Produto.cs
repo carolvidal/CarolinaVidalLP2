fb35542c27e14857b4ac9a4a68f4a8097c4b165b
Carol Vidal <carolvidal931@gmail.com>
2017-09-22 17:10:33 -0300

    Exercício Produtos adicionado.

---
+++ 2C/5- Produtos/Produto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5__Produtos
+{
+    class Produto
+    {
+        public string id { get; private set; }
+        public string nome { get; private set; }
+        public int QtdEstoque { get; private set; }
+        public double preço { get; set; }
+        public string marca { get; }
+
+        public Produto (string i, string n, double p) {
+
+        id = i;
+        nome = n;
+        preço = p;
+        QtdEstoque = 0;
+
+        }
+
+        public void Repor (int NovaQtd) {
+
+        QtdEstoque += NovaQtd;
+
+        }
+
+        public void Retirar (int valor) {
+
+            if (QtdEstoque - valor > QtdEstoque)
+                throw new System.InvalidOperationException("Operação inválida!");
+            else
+                QtdEstoque -= valor;
+        }
+
+        public void Imprimir() {
+
+        Console.WriteLine("Produto: {0} - R${1:0.00} - Estoque: {2}", nome, preço, QtdEstoque);
+
+        }
+
+        
+
+    }
+}
---
+++ 2C/5- Produtos/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5__Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe os dados do produto:");
+
+            Console.WriteLine("Informe o ID: ");
+            string id = Console.ReadLine();
+
+            Console.WriteLine("Informe o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Informe o preço: ");
+            double preço = double.Parse(Console.ReadLine());
+
+            Produto p = new Produto (id, nome, preço);
+
+            int x = 4;
+
+            while (x != 0)
+            {
+                Console.WriteLine(" 1- Reposição de estoque\n 2- Retirada de estoque\n 3- Informações do produto\n 0- Sair");
+                x = int.Parse(Console.ReadLine());
+
+                if (x == 1)
+                {
+                    Console.WriteLine("Valor a ser reposto:");
+                    p.Repor(int.Parse(Console.ReadLine()));
+                }
+
+                else if (x == 2)
+                {
+                    Console.WriteLine("Valor a ser retirado:");
+                    p.Retirar(int.Parse(Console.ReadLine()));
+                }
+
+                else if (x == 3)
+                {
+                    p.Imprimir();
+                }
+
+                else if (x == 0)
+                {
+                    break;
+                }
+
+                else
+                    throw new System.InvalidOperationException("Comando inválido!");
+            }
+        }
+    }
+}
